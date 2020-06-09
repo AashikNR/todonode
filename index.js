390948d44bf10137e919dd02f78cf2bddd163a67
AashikNR <ashiknizam77@gmail.com>
2020-06-09 11:46:15 +0530

    todonodejs

--- index.js
+++ index.js
@@ -33,8 +33,40 @@ var obj = {
 }
 
 
-var edit = (val)=>{
-    console.log(val);
+var edit = (val,new_value)=>{
+    var data = fs.readFileSync('myjsonfile.json');
+    var remove = val;
+    var json = JSON.parse(data);
+
+    for (var i = 0; i < json.table.length ; i++) {
+        if (json.table[i].value == val)
+            var id_var = json.table[i].id
+        }
+
+    var table = json.table;
+    json.table = table.filter((table) => { return table.value !== remove });
+    fs.writeFileSync('myjsonfile.json', JSON.stringify(json, null, 2));
+    console.log("Edited Successfully")
+
+
+
+
+    var obj = {
+        table: []
+     };
+     var json = JSON.stringify(obj);
+     fs.readFile('myjsonfile.json', 'utf8', (err, data)=>{
+        if (err){
+            console.log(err);
+        } else {
+        obj = JSON.parse(data);
+        obj.table.push({id:id_var, value:new_value});
+        json = JSON.stringify(obj);
+        fs.writeFile('myjsonfile.json', json, 'utf8', (err) => {
+            if (err) throw err;
+        });
+    }});
+
 }
 
 var remove = (val)=>{
@@ -63,8 +95,8 @@ if ((test[0] ===  "add")&&(typeof test[1] !== 'undefined')) {
     add(test[1]);
 } else if ((test[0] === "delete")&&(typeof test[1] !== 'undefined')){
     remove(test[1]);
-} else if  ((test[0] === "edit")&&(typeof test[1] !== 'undefined')) {
-    edit(test[1]);
+} else if  ((test[0] === "edit")&&(typeof test[1] !== 'undefined')&&(typeof test[2] !== 'undefined')) {
+    edit(test[1],test[2]);
 } else if (test[0] === "list") {
     list();
 } else {
